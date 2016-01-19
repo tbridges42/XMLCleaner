7c80c8ec77605549aa79525802a2ad1f42e80555
Tony <tbridges42@gmail.com>
2016-01-18 21:09:04 -0600

    comment out broken test

--- test_cleaner.py
+++ test_cleaner.py
@@ -37,49 +37,49 @@ class TestCleaner(unittest.TestCase):
 
   #  def test_add_to_set_unique(self):
 
-
-    def test_duplicate_xml(self):
-        test_root = ET.Element("Purchases")
-        purchase = ET.SubElement(test_root, "Purchase")
-        ET.SubElement(purchase, "Description").text = "A unique purchase"
-        for i in range(0, 4):
-            purchase = ET.SubElement(test_root, "Purchase")
-            ET.SubElement(purchase, "Description").text = "A duplicate purchase"
-        for i in range(0, 4):
-            purchase = ET.SubElement(test_root, "Purchase")
-            ET.SubElement(purchase, "Description").text = "A complicated duplicate purchase"
-            ET.SubElement(purchase, "ExactValue").text = "15651231.54"
-            ET.SubElement(purchase, "NIGPCode").text = "94511"
-        purchase = ET.SubElement(test_root, "Purchase")
-        ET.SubElement(purchase, "Description").text = "A unique purchase in canon"
-        for i in range(0, 4):
-            purchase = ET.SubElement(test_root, "Purchase")
-            ET.SubElement(purchase, "Description").text = "A complicated duplicate purchase in canon"
-            ET.SubElement(purchase, "ExactValue").text = "15651231.54"
-            ET.SubElement(purchase, "NIGPCode").text = "94511"
-        test = ET.ElementTree(test_root)
-
-        canon_root = ET.Element("Purchases")
-        purchase = ET.SubElement(test_root, "Purchase")
-        ET.SubElement(purchase, "Description").text = "A unique purchase in canon"
-        purchase = ET.SubElement(test_root, "Purchase")
-        ET.SubElement(purchase, "Description").text = "A complicated duplicate purchase in canon"
-        ET.SubElement(purchase, "ExactValue").text = "15651231.54"
-        ET.SubElement(purchase, "NIGPCode").text = "94511"
-        canon = ET.ElementTree(canon_root)
-
-        gold_root = ET.Element("Purchases")
-        purchase = ET.SubElement(test_root, "Purchase")
-        ET.SubElement(purchase, "Description").text = "A unique purchase"
-        purchase = ET.SubElement(test_root, "Purchase")
-        ET.SubElement(purchase, "Description").text = "A duplicate purchase"
-        purchase = ET.SubElement(test_root, "Purchase")
-        ET.SubElement(purchase, "Description").text = "A complicated duplicate purchase"
-        ET.SubElement(purchase, "ExactValue").text = "15651231.54"
-        ET.SubElement(purchase, "NIGPCode").text = "94511"
-        gold = ET.ElementTree(gold_root)
-
-        self.equal_xml(cleaner.remove_duplicates(test, canon), gold)
+    # TODO: Fix this
+    # def test_duplicate_xml(self):
+    #     test_root = ET.Element("Purchases")
+    #     purchase = ET.SubElement(test_root, "Purchase")
+    #     ET.SubElement(purchase, "Description").text = "A unique purchase"
+    #     for i in range(0, 4):
+    #         purchase = ET.SubElement(test_root, "Purchase")
+    #         ET.SubElement(purchase, "Description").text = "A duplicate purchase"
+    #     for i in range(0, 4):
+    #         purchase = ET.SubElement(test_root, "Purchase")
+    #         ET.SubElement(purchase, "Description").text = "A complicated duplicate purchase"
+    #         ET.SubElement(purchase, "ExactValue").text = "15651231.54"
+    #         ET.SubElement(purchase, "NIGPCode").text = "94511"
+    #     purchase = ET.SubElement(test_root, "Purchase")
+    #     ET.SubElement(purchase, "Description").text = "A unique purchase in canon"
+    #     for i in range(0, 4):
+    #         purchase = ET.SubElement(test_root, "Purchase")
+    #         ET.SubElement(purchase, "Description").text = "A complicated duplicate purchase in canon"
+    #         ET.SubElement(purchase, "ExactValue").text = "15651231.54"
+    #         ET.SubElement(purchase, "NIGPCode").text = "94511"
+    #     test = ET.ElementTree(test_root)
+    #
+    #     canon_root = ET.Element("Purchases")
+    #     purchase = ET.SubElement(test_root, "Purchase")
+    #     ET.SubElement(purchase, "Description").text = "A unique purchase in canon"
+    #     purchase = ET.SubElement(test_root, "Purchase")
+    #     ET.SubElement(purchase, "Description").text = "A complicated duplicate purchase in canon"
+    #     ET.SubElement(purchase, "ExactValue").text = "15651231.54"
+    #     ET.SubElement(purchase, "NIGPCode").text = "94511"
+    #     canon = ET.ElementTree(canon_root)
+    #
+    #     gold_root = ET.Element("Purchases")
+    #     purchase = ET.SubElement(test_root, "Purchase")
+    #     ET.SubElement(purchase, "Description").text = "A unique purchase"
+    #     purchase = ET.SubElement(test_root, "Purchase")
+    #     ET.SubElement(purchase, "Description").text = "A duplicate purchase"
+    #     purchase = ET.SubElement(test_root, "Purchase")
+    #     ET.SubElement(purchase, "Description").text = "A complicated duplicate purchase"
+    #     ET.SubElement(purchase, "ExactValue").text = "15651231.54"
+    #     ET.SubElement(purchase, "NIGPCode").text = "94511"
+    #     gold = ET.ElementTree(gold_root)
+    #
+    #     self.equal_xml(cleaner.remove_duplicates(test, canon), gold)
 
     def test_cleaner(self):
         self.reset()
