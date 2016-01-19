b221282d78563d8da163d76a26eb9d1a91d18ccf
bridgtxcdf <tbridges42@gmail.com>
2016-01-19 12:02:03 -0600

    Add merge and tests

---
+++ merge.py
@@ -0,0 +1,39 @@
+import cleaner
+import time
+import os
+from os import walk
+
+
+# get all files to be merged
+def getunmergedfiles():
+    files = []
+    for (dirpath, dirnames, filenames) in walk(os.path.dirname(os.path.abspath(__file__))):
+        for (filename) in filenames:
+            if filename.startswith('upload') and filename.endswith('.xml'):
+                files.append(filename)
+        break
+    return files
+
+
+def main():
+    agencies = {}
+    for (filename) in getunmergedfiles():
+        # the agency abbreviation is the last section of a hyphen-separated filename
+        agency = filename.split('-')[-1].split('.')[0]
+        # one agency has an additional "85" in the last section
+        if agency == '85':
+            agency = filename.split('-')[-2].split('.')[0]
+        print(agency)
+        agencyset = set()
+        if agency in agencies:
+            agencyset = agencies[agency]
+        cleaner.add_to_set(agencyset, cleaner.to_xml(filename))
+        agencies[agency] = agencyset
+
+    for (agency) in agencies:
+        print(agency)
+        cleaner.print_set(agencies[agency], "upload-" + agency + "-" + time.strftime("%y%m%d") + "-merged.xml", "wb")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_merge.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestMerger(unittest.TestCase):
+
+    def test_merge(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
